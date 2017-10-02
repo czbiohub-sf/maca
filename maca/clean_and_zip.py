4a1fd6b9815359a4de741938917f6869f631c2aa
olgabot <olga.botvinnik@czbiohub.org>
2017-10-02 15:27:33 -0700

    deal with matrices that are x-by-cell instead of cell-by-x

--- maca/clean_and_zip.py
+++ maca/clean_and_zip.py
@@ -25,6 +25,10 @@ HTSEQ_METADATA_ROWS = ['__alignment_not_unique',
  '__not_aligned',
  '__too_low_aQual']
 
+COUNTS_SUFFIX = '.htseq-count.csv'
+METADATA_SUFFIX = '.log.csv'
+CELL_DIMENSION = 'columns'
+
 
 def clean_htseq_mapping_stats(htseq, mapping_stats):
     """Remove metadata-type columns from htseq and combine with mapping_stats
@@ -53,10 +57,32 @@ def make_basename(prefix, output_format):
     return prefix + '.' + output_format
 
 
-def read_csv(csv):
+def read_csv(csv, cell_dimension=CELL_DIMENSION):
+    """Returns a feature-by-cell matrix
+    
+    By providing the cell dimension, this function will ALWAYS return a 
+    feature-by-cell matrix 
+    
+    Parameters
+    ----------
+    csv : str
+        Filename of the data to read
+    cell_dimension : "row" | "col"
+        Which dimension contains the cells
+    """
+    row = cell_dimension.startswith('row')
+    col = cell_dimension.startswith('col')
+
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        return pd.read_csv(csv, index_col=0)
+        table = pd.read_csv(csv, index_col=0)
+
+    # Transpose the table if cells are on the columns and not the rows
+    if col and not row:
+        table = table.T
+
+    # Return a table with the cells as the columns
+    return table
 
 
 def write_counts_metadata(counts, metadata, folder, platename,
@@ -114,7 +140,8 @@ def write_counts_metadata(counts, metadata, folder, platename,
 
 
 @click.command()
-@click.option('--input-folder', default='./')
+@click.argument('input_folder', nargs=1,
+                type=click.Path(dir_okay=True, readable=True))
 @click.option('--output-folder', default='./',
               help='Write the cleaned data to this folder')
 @click.option('--platename', default=None,
@@ -127,32 +154,56 @@ def write_counts_metadata(counts, metadata, folder, platename,
 @click.option('--zipped', is_flag=True,
               help="If added, make a zip file containing both the counts and "
                    "metadata")
+@click.option('--counts-suffix', default=COUNTS_SUFFIX,
+              default="String at the end of the filename that indicates it is "
+                      "a counts matrix, e.g. integers of read counts mapping "
+                      "to genes for each cell. Must exactly match the end of "
+                      "the file after a plate name, e.g. if there is a period "
+                      "separating the plate name and this suffix, the suffix "
+                      "should contian the period")
+@click.option('--metadata-suffix', default=METADATA_SUFFIX,
+              default="String at the end of the filename that indicates it is "
+                      "a cell metadata file, e.g. number of reads per cell or "
+                      "percent mapped reads. Must exactly match the end of the"
+                      " file after a plate name, e.g. if there is a period "
+                      "separating the plate name and this suffix, the suffix "
+                      "should contian the period")
+@click.option('--cell-dimension', default=CELL_DIMENSION,
+              help="Specifies whether the cells are the rows or the columns "
+                   "on the input files. Default is 'row', valid values are "
+                   "'rows', 'cols', 'col', 'columns'")
 def clean_and_zip(input_folder, output_folder, platename=None,
-                  output_format='csv', rstats=False, zipped=False):
+                  output_format='csv', rstats=False, zipped=False,
+                  counts_suffix=COUNTS_SUFFIX, metadata_suffix=METADATA_SUFFIX,
+                  cell_dimension=CELL_DIMENSION):
+    """Combines counts and metadata files into single zipped plates files
+    
+    Always outputs features-by-cell matrices
+    """
     if output_folder is not None and not os.path.exists(output_folder):
         os.mkdir(output_folder)
 
     if platename is not None:
         print(f'Reading plate {platename}...')
-        csv = os.path.join(input_folder, f'{platename}.htseq-count.csv')
-        htseq = read_csv(csv)
+        csv = os.path.join(input_folder, f'{platename}{counts_suffix}')
+        htseq = read_csv(csv, cell_dimension)
 
-        csv = os.path.join(input_folder, f'{platename}.log.csv')
-        mapping_stats = read_csv(csv).dropna(how='all')
+        csv = os.path.join(input_folder, f'{platename}{metadata_suffix}')
+        mapping_stats = read_csv(csv, cell_dimension).dropna(how='all')
 
         counts, metadata = clean_htseq_mapping_stats(htseq, mapping_stats)
         write_counts_metadata(counts, metadata, output_folder, platename,
                               output_format, rstats, zipped)
     
     else:
-        for csv in glob.iglob(os.path.join(input_folder, '*.htseq-count.csv')):
+        for csv in glob.iglob(os.path.join(input_folder, '*{counts_suffix}')):
             platename = os.path.basename(csv).split('.')[0]
             print(f'Reading plate {platename}...')
             
-            htseq = read_csv(csv)
+            htseq = read_csv(csv, cell_dimension)
 
-            csv = os.path.join(input_folder, f'{platename}.log.csv')
-            mapping_stats = read_csv(csv).dropna(how='all')
+            csv = os.path.join(input_folder, f'{platename}{metadata_suffix}')
+            mapping_stats = read_csv(csv, cell_dimension).dropna(how='all')
 
             counts, metadata = clean_htseq_mapping_stats(htseq, mapping_stats)
 
